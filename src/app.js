291ee7646f3a8733da6e9af016cef180d7384c1a
Gabriel Bergmann <gabriel.bergmann@senior.com.br>
2019-09-16 19:33:22 -0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+'use strict'
+var React = require('react')
+var createReactClass = require('create-react-class')
+
+var Title = createReactClass({
+    render: function () {
+        return React.createElement('h1', null, 'Título');
+    }
+})
+
+module.exports = Title
